2c5b01769dece5d2cea7bf72cd0ec0c5c28c31f0
Richo <daininoricho@yahoo.com>
2025-01-10 21:08:43 +0800

    chore: initialize spring boot

---
+++ src/main/java/com/richodainino/galaxy_store/GalaxyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.richodainino.galaxy_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyStoreApplication.class, args);
+	}
+
+}
