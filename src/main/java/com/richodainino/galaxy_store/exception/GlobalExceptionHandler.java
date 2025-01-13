3a52e41c0c3b2ae71cee5077a8d1b494fa609edd
Richo <daininoricho@yahoo.com>
2025-01-13 20:05:13 +0800

    chore: add error log from global exception handler

--- src/main/java/com/richodainino/galaxy_store/exception/GlobalExceptionHandler.java
+++ src/main/java/com/richodainino/galaxy_store/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.richodainino.galaxy_store.exception;
 import com.richodainino.galaxy_store.handler.ResponseHandler;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,17 +13,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // Handle not found exception with response
     @ExceptionHandler(value = ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        log.error("ResourceNotFoundException: " + HttpStatus.NOT_FOUND + " " + ex.getMessage());
         return ResponseHandler.generateResponse(HttpStatus.NOT_FOUND, ex.getMessage(), null);
     }
 
     // Handle entity model validation
     @ExceptionHandler(value = ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex){
+        log.error("ConstraintViolationException: " + HttpStatus.BAD_REQUEST + " " + ex.getMessage());
         List<String> errorMessages = new ArrayList<>();
         for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
             errorMessages.add(violation.getMessage());
